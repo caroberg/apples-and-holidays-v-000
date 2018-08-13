76529db8b77bec558102539f051ac31d7b2ef357
Caroline Berg <carolineeberg@gmail.com>
2018-08-13 17:23:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall][:columbus_day] = ["Leaf Piles", "No School"]
+  holiday_hash[:fall][:columbus_day] = ["Flags", "Italian Food", Parade Floats]
 end
 
 def all_winter_holiday_supplies(holiday_hash)
