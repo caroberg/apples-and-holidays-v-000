157fd56afd31d7a633a0216d7c7f947bb66e03b6
Caroline Berg <carolineeberg@gmail.com>
2018-08-14 01:11:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,8 +44,8 @@ def all_supplies_in_holidays(holiday_hash)
 
       puts "#{season.capitalize}:"
         puts "  #{day.capitalize}: #{item}"
-    end
-  end
+    
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
